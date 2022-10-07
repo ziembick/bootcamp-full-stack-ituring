024438a9027182b3d2c47c82304f6c78fd642646
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-07 20:02:59 -0300

    mensagem de Whatsapp com template strings

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -3,3 +3,5 @@ const email = "pauloziembick@gmail.com"
 const profissao = "Programador"
 const idade = 27
 const lugarSonhoDeConhecer = "Inglaterra"
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarSonhoDeConhecer}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
