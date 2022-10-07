29ab8a71ee7bf7e9213a6675278e8df14f71991a
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-07 12:19:20 -0300

    adicionando primeiras infos no pratica-1

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -0,0 +1,11 @@
+const nome = "Paulo Ziembick";
+const email = "pauloziembick@gmail.com";
+const profissao = "Programador";
+const idade = 27;
+const lugarSonhoDeConhecer = "Algum lugar da Europa"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugarSonhoDeConhecer)
