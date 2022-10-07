c527f897ac7b4273258b76e92c1361958e59417c
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-07 18:52:18 -0300

    adicionando código sobre ser true ou false para string

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+const nome = "Paulo Ziembick";
+const email = "pauloziembick@gmail.com";
+const profissao = "Programador";
+const idade = 27;
+const lugarSonhoDeConhecer = "Algum lugar da Europa"
+
+console.log(typeof(nome) == "string")
+console.log(typeof(email) == 'string')
+console.log(typeof(profissao) == 'string')
+console.log(typeof(idade) == 'string')
+console.log(typeof(lugarSonhoDeConhecer) == 'string')
