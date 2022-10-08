21d0ebfa5df67e65f330b0d27c48d6b689bf9a3f
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-07 22:08:23 -0300

    tabuada do 123

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -0,0 +1,34 @@
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+let numero5 = 5
+let numero6 = 6
+let numero7 = 7
+let numero8 = 8
+let numero9 = 9
+let numero10 = 10
+
+let tabuada = 123
+
+numero1 = numero1 * tabuada
+numero2 = numero2 * tabuada
+numero3 = numero3 * tabuada
+numero4 = numero4 * tabuada
+numero5 = numero5 * tabuada
+numero6 = numero6 * tabuada
+numero7 = numero7 * tabuada
+numero8 = numero8 * tabuada
+numero9 = numero9 * tabuada
+numero10 = numero10 * tabuada
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+console.log(numero4)
+console.log(numero5)
+console.log(numero6)
+console.log(numero7)
+console.log(numero8)
+console.log(numero9)
+console.log(numero10)
