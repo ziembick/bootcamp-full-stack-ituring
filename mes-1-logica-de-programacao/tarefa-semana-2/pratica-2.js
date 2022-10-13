4122b68a3b94b4f76390a1abd50f8808892fbb10
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-13 14:09:37 -0300

    função 2 do pratica-2

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -3,3 +3,10 @@ function nomeComprador (nome) {
 }
 
 nomeComprador("Paulo")
+
+function entregaProduto (produto, endereco, recebimento) {
+    
+    console.log(`Fizemos a entrega do ${produto} no ${endereco} com sucesso! \nO seu produto foi recebido por: ${recebimento}. \nEsperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.`)
+}
+
+entregaProduto("iPhone", "Av. Damasceno Vieira, 1080", "Paulo")
