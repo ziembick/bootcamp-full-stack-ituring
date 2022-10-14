8dc6772e7231da432b8b7558d24b21117a9348f1
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-14 13:53:05 -0300

    arrow function pratica-6

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-6.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-6.js
@@ -1,4 +1,8 @@
-const obterAnoAtual = () => {
-    //statements
+const calcularDesconto = (valor, desconto) => {
+    const valorFinal = valor*(1 - desconto/100)
+    return valorFinal.toFixed(2)
 }
 
+const valorComDesconto = calcularDesconto(100, 10)
+
+console.log(valorComDesconto)
