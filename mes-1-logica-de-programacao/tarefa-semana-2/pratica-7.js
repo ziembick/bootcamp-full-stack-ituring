041cce6bdf269d17007d162c1a8f8c72ce75c428
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-15 16:08:06 -0300

    linguagens pratica-7

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,11 @@
+linguagem = "es"
+
+if (linguagem =="es") {
+    console.log("Hola Mundo")
+} else if (linguagem == "en") {
+    console.log("Hello World")
+} else if (linguagem == "pt") {
+    console.log("Olá mundo")
+} else {
+    console.log ("Olá mundo")
+}
