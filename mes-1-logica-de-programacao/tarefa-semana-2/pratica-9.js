ed63b0379f3d7443b8da1e9db63dbd1ab37bd156
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-15 18:39:21 -0300

    atualizando pratica-9

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
@@ -1,10 +1,19 @@
-numero = 15
-
-const numero1 = 10
-const numero2 = 20
-
-if (numero == numero1 && numero2) {
-    console.log(numero2)
-} else {
-    console.log(numero1 || numero2)
+function obterNumero (numero){
+    const numero1 = 10
+    const numero2 = 20
+    
+    if(numero == numero1) {
+        console.log(numero2)
+    } else if (numero == numero2) {
+        console.log(numero2)
+    } else if (numero == numero1 && numero == numero2){
+        console.log(numero1 || numero2)
+    } else {
+        console.log(numero)
+    }
 }
+
+obterNumero(10)
+obterNumero(10)
+
+
