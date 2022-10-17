f10cc8902c51db4a7de340a7010fc250a7ef274d
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-17 11:52:15 -0300

    adicionando imc pratica 14

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-14.js
@@ -0,0 +1,17 @@
+function obterImc (imc) {
+    const peso = 90
+    const altura = 1.90
+    imc = peso / (altura * altura)
+
+    if (imc < 18.5) {
+        console.log("Abaixo do Peso")
+    } else if (imc >= 18.5 && imc <= 25) {
+        console.log("Peso Normal")
+    } else if (imc >= 25 && imc <= 30) {
+        console.log("Sobrepreso") 
+    } else if (imc > 30) {
+        console.log("Obesidade")
+    }   
+}
+
+obterImc()
