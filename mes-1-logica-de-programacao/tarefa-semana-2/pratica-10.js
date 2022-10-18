0efcffb236c6c0c0a72940ae9527bd4d4646a96d
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-18 19:38:55 -0300

    atualizando pratica 9 e 10

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-10.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-10.js
@@ -1,19 +1,13 @@
-function obterNumero (numero){
-    const numero1 = 10
-    const numero2 = 20
-    const numero3 = 30
-    
-    if(numero === numero1) {
-        console.log(numero3)
-    } else if (numero === numero2) {
-        console.log(numero3)
-    } else if (numero === numero3) {
-        console.log(numero3)
-    } else if (numero === numero1 && numero === numero2 && numero === numero3) {
-        console.log(numero1 || numero2 || numero3)
+function obterNumero2 (n1, n2, n3) {
+    if (n1 > n2 && n1 > n3) {
+        console.log(n1)
+    } else if (n2 > n3) {
+        console.log(n2)
+    } else {
+        console.log(n3)
     }
 }
 
-obterNumero(10)
-obterNumero(20)
-obterNumero(30)
+obterNumero2(1, 1 ,1)
+obterNumero2(1, 2 ,1)
+obterNumero2(3, 2 ,1)
--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
@@ -1,17 +1,9 @@
-function obterNumero (numero){
-    const numero1 = 10
-    const numero2 = 20
-    
-    if(numero === numero1) {
-        console.log(numero2)
-    } else if (numero === numero2) {
-        console.log(numero2)
-    } else if (numero === numero1 && numero === numero2) {
-        console.log(numero1 || numero2)
-    } 
+function obterNumero (n1, n2) {
+    if (n1 > n2) {
+        console.log(n1)
+    } else {
+        console.log(n2)
+    }
 }
 
-obterNumero(10)
-obterNumero(20)
-
-
+obterNumero(10, 20)
