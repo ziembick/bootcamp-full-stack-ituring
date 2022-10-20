32cfd1bb497486dbc4f537a6f94ff1413dee080b
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-20 20:41:03 -0300

    condição dos numeros primos

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-3.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-3.js
@@ -1,30 +1,12 @@
-function exibirNumeroPrimos (n) {
-    let i = 1
-
-    while (i <= n ) {
-        if ( i % 1 === 0){
-            return
-        } else if (i % n === 0) {
-            return
-        } else {
-            return
+function obterNumerosPrimos (n) {
+    let i = 2
+    
+    while(/* condição de parada do loop*/) {
+        if (/*condição*/) {
+            return false;
         }
         i++
-        return exibirNumeroPrimos
     }
 
-    if (n < 0) {
-        console.log("Número Inválido")
-        return undefined
-    }
+    return true;
 }
-
-exibirNumeroPrimos(17)
-exibirNumeroPrimos(2)
-exibirNumeroPrimos(7)
-exibirNumeroPrimos(8)
-exibirNumeroPrimos(5)
-exibirNumeroPrimos(23)
-exibirNumeroPrimos(15)
-exibirNumeroPrimos(1500450271)
-
