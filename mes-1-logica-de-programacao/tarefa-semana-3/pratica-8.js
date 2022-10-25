5b1f7598cfe90250485b01dbc8013327d36f961c
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-25 18:56:22 -0300

    pratica 8 e 9 solução

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
@@ -31,4 +31,26 @@ let soma = numeros.reduce(function(soma,i) {
 
 console.log(`O saldo final de sua conta foi positivo no valor de R$${soma}`)
 
+//solução
+let exibeExtrato = lista => {
+    let saldoFinal = 0
+    let totalDepositos = 0
+
+    for (item of lista) {
+        totalDepositos += item > 0 ? item : 0
+        saldoFinal += item
+    }
+
+    const positivoOuNegativo = saldoFinal > 0 ? "positivo" : "negativo"
+
+    console.log(`O valor total depositado foi de: R$ ${totalDepositos}`)
+    console.log(`O valor total retirado foi de: R$ ${totalDepositos - saldoFinal}`)
+    console.log(`O saldo final de sua conta foi ${positivoOuNegativo} no valor de R$ ${Math.abs(saldoFinal)}`)
+}
+
+let listaDepositosRetiradas = [100, -20, -30, 10, -7, -21, -5]
+exibeExtrato(listaDepositosRetiradas)
+listaDepositosRetiradas = [-100, -20, -30, 10, -7, -21, -5]
+exibeExtrato(listaDepositosRetiradas)
+
 
--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-9.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-9.js
@@ -46,3 +46,31 @@ let soma = numeros.reduce(function(soma,i) {
 console.log(`O saldo final de sua conta foi positivo no valor de R$${soma}`)
 
 
+// solução
+
+let exibeExtratoCompleto = lista => {
+    let saldoFinal = 0
+    let totalDepositos = 0
+    let qtdDepositos = 0
+    let qtdRetiradas = 0
+
+    for (item of lista) {
+        const ehDeposito = item > 0
+        qtdDepositos += ehDeposito ? 1 : 0
+        qtdRetiradas += !ehDeposito ? 1 : 0
+        totalDepositos += ehDeposito ? item : 0
+        saldoFinal += item
+    }
+    const positivoOuNegativo = saldoFinal > 0 ? "positivo" : "negativo"
+
+    console.log(`Total de depósitos: ${qtdDepositos}`)
+    console.log(`Total de retiradas: ${qtdRetiradas}`)
+    console.log(`O valor total depositado foi de: R$ ${totalDepositos}`)
+    console.log(`O valor total retirado foi de: R$ ${totalDepositos - saldoFinal}`)
+    console.log(`O saldo final de sua conta foi ${positivoOuNegativo} no valor de R$ ${Math.abs(saldoFinal)}`)
+}
+
+let listaDepositosRetiradas = [100, -20, -30, 10, -7, -21, -5]
+exibeExtratoCompleto(listaDepositosRetiradas)
+listaDepositosRetiradas = [-100, -20, -30, 10, -7, -21, -5]
+exibeExtratoCompleto(listaDepositosRetiradas)
