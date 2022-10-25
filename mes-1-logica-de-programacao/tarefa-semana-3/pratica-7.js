3851cab3c2a5e8dd7edca1f72779d006ba3741ff
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-25 18:42:32 -0300

    atualizando soluções

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
@@ -1,3 +1,4 @@
+//minha
 let numeros = [100, -20, -30, 10, -7, -21, -5]
 
 let soma = numeros.reduce(function(soma,i) {
@@ -5,3 +6,18 @@ let soma = numeros.reduce(function(soma,i) {
 })
 
 console.log(`O saldo final de sua conta foi positivo no valor de R$${soma}`)
+
+//solução
+let exibeSaldoFinal = lista => {
+    let saldoFinal = 0
+    for (item of lista)
+        saldoFinal += item
+    const positivoOuNegativo = saldoFinal > 0 ? "positivo" : "negativo"
+    console.log(`O saldo final de sua conta foi ${positivoOuNegativo} no valor de R$ ${Math.abs(saldoFinal)}.`)
+}
+
+/* TESTANDO A SOLUÇÃO */
+let listaDepositosRetiradas = [100, -20, -30, 10, -7, -21, -5]
+exibeSaldoFinal(listaDepositosRetiradas)
+listaDepositosRetiradas = [-100, -20, -30, 10, -7, -21, -5]
+exibeSaldoFinal(listaDepositosRetiradas)
