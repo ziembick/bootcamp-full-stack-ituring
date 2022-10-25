6f7de1ec2fdfe7b47bf0613c8e7d3dda075f547c
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-25 13:06:18 -0300

    pratica 7

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
@@ -1,7 +1,7 @@
 let numeros = [100, -20, -30, 10, -7, -21, -5]
 
-let soma = numeros.reduce(function(acumulador,valor) {
-    return  acumulador + valor;
+let soma = numeros.reduce(function(soma,i) {
+    return  soma + i;
 })
 
 console.log(`O saldo final de sua conta foi positivo no valor de R$${soma}`)
