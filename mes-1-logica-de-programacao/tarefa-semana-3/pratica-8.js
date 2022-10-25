1edf7f2e1eb4c303142c6418a242e51f92425da9
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-25 13:22:49 -0300

    pratica 8

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
@@ -0,0 +1,34 @@
+function somar(lista) {
+    let total = 0;
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i] >= 2) {
+            total += lista[i];
+        } 
+    }
+    return total;
+}
+let lista = [100, -20, -30, 10, -7, -21, -5];
+console.log(somar(lista));
+
+function somar2(lista2) {
+    let total = 0;
+    for (let i = 0; i < lista2.length; i++) {
+        if (lista2[i] <= 0) {
+            total += lista2[i];
+        }
+    }
+    return total;
+}
+let lista2 = [100, -20, -30, 10, -7, -21, -5];
+console.log(somar2(lista2));
+
+//
+let numeros = [100, -20, -30, 10, -7, -21, -5]
+
+let soma = numeros.reduce(function(soma,i) {
+    return  soma + i;
+})
+
+console.log(`O saldo final de sua conta foi positivo no valor de R$${soma}`)
+
+
