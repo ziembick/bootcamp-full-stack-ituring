402440f999932e9b484b3aa8c760f09f8cd30b5d
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-27 14:50:47 -0300

    lista seriados pratica 2

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-2.js
@@ -0,0 +1,13 @@
+let listaSeriados = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+
+function buscaSeriado(listaSeriados, seriado) {
+    let indice = undefined;
+    listaSeriados.forEach((listaSeriados, i) => {
+      if (listaSeriados === seriado) {
+        indice = i;
+      }
+    });
+    return indice;
+  }
+
+console.log(buscaSeriado(listaSeriados,"Dexter"))
