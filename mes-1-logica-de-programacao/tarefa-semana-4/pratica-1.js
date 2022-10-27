076fbf04d4c3134c3b43e683712c44f881badb30
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-27 12:18:19 -0300

    lista de seriados com forEach na pratica 1

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-1.js
@@ -0,0 +1,5 @@
+let listaSeriados = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+
+listaSeriados.forEach(function(item, i) {
+    console.log(`[${i}] ${item}`)
+})
