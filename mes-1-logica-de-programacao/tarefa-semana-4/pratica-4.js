c028ba76aa6cae7a65bcbf50d4c42bc6a25d073f
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-28 17:36:17 -0300

    raiz quadrada parte 1

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-4.js
@@ -0,0 +1,23 @@
+let numeros = [4, 9, 16, 25, 36, 49];
+
+let listaRaiz = numeros.map(item => (Math.sqrt(item)))
+
+
+console.log(listaRaiz);
+////////////////////////////////////////////////
+let numbers = [4, 9, 16, 25, 36, 49];
+
+let listaRaizQuadrada = numbers.map(item => (Math.sqrt(item)))
+
+console.log(listaRaizQuadrada)
+///////////////////////////////////////
+
+
+let listaExemplo = [1, 5, 2]
+let novaLista = listaExemplo.map(function(item) {
+    return item
+})
+
+console.log(novaLista)
+
+
