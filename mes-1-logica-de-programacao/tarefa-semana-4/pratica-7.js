7bae5e5e8b0a133640db3f15723ad6393045254c
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-29 17:56:41 -0300

    pratica 7 resulução 1

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-7.js
@@ -0,0 +1,9 @@
+const hasPaid = {
+    marca: "Tramontina",
+    produto: "Jogo de talheres",
+    preco: 500,
+    quantidade: 132,
+    categoria: "Utensilhos domésticos"
+}
+
+console.log(Object.keys(hasPaid))
