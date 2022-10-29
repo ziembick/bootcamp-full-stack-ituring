e88f126fe1fabebf95e11c243b13422739462740
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-29 17:27:57 -0300

    filme

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-6.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-6.js
@@ -1,12 +1,14 @@
 let filme = {
-        titulo: "Estrelhas Além do Tempo",
-        notaAvaliacao: 7.8,
-        duracao: 127,
-        anoPublicacao: 2016,
-        categoria: "Drama"
+    titulo: "Estrelas Além do Tempo",
+    notaAvaliacao: 7.8,
+    duracao: 127,
+    anoPublicacao: 2016,
+    categoria: "Drama"
 }
 
 console.log(`O filme ${filme.titulo} que estreou em ${filme.anoPublicacao} tem avaliação ${filme.notaAvaliacao}pelo IMDb.`)
+///////////////////////////////////////////////////////////////
+
 
 
 
