385b9d6e4b6099f2014cb3598129423e16bb28fb
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-31 18:07:10 -0300

    adicionando pratica 8

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-8.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-8.js
@@ -1,23 +1,4 @@
-let lista = {
-    titulo: "Estrelas Além do Tempo",
-    notaAvaliacao: 7.8,
-    duracao: 127,
-    anoPublicacao: 2016,
-    genero: ["Biografia", "Drama", "História"],
-    
-}
-
-  console.log(`${lista.titulo} (Nota: ${lista.notaAvaliacao})\n• Duração: ${lista.duracao} minutos\n• Ano de publicação: ${lista.anoPublicacao}\n• Gênero: ${lista.genero}`)
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-const obterTitulos = (lista2) => {
-  console.log(`${titulo} e ${notaAvaliacao}`)
-}
-
-let lista2 = [{
+let listaFilmes = [{
   titulo: "Estrelas Além do Tempo",
   notaAvaliacao: 7.8,
   duracao: 127,
@@ -38,4 +19,14 @@ let lista2 = [{
   anoPublicacao: 2014,
   genero: ["Biografia", "Drama", "Suspense", "Guerra"]
 }]
+
+
+listaFilmes.forEach(function(item){
+  console.log(`Titulo ${item.titulo}`)
+})
+
+
+
+
+
 
