92fced5af69849d19600b8108d0ed10d3f58cfa4
Paulo Ziembick <pauloziembick@gmail.com>
2022-10-31 21:31:46 -0300

    math.max

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-9.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-9.js
@@ -1,26 +1,41 @@
 let listaFilmes = [{
-    titulo: "Estrelas Além do Tempo",
-    notaAvaliacao: 7.8,
-    duracao: 127,
-    anoPublicacao: 2016,
-    genero: ["Biografia", "Drama", "História"]
-  },
-  {
-    titulo: "Top Gun",
-    notaAvaliacao: 8.6,
-    duracao: 130,
-    anoPublicacao: 2022,
-    genero: ["Ação", "Drama"]
-  },
-  {
-    titulo: "O Jogo da Imitação",
-    notaAvaliacao: 8.0,
-    duracao: 114,
-    anoPublicacao: 2014,
-    genero: ["Biografia", "Drama", "Suspense", "Guerra"]
-  }]
+  titulo: "Um Sonho de Liberdade",
+  notaAvaliacao: 9.2,
+  duracao: 142,
+  anoPublicacao: 1994,
+  categoria: "Drama"
+},
+{
+  titulo: "Clube da Luta",
+  notaAvaliacao: 8.7,
+  duracao: 139,
+  anoPublicacao: 1999,
+  categoria: "Drama"
+},
+{
+  titulo: "Toy Story 3",
+  notaAvaliacao: 8.3,
+  duracao: 103,
+  anoPublicacao: 2010,
+  categoria: "Animação"
+}]
 
-const obterFilmes = (listaFilmes) => {
+
+const obterNota = (listaFilmes) => {
+  listaFilmes.filter(function(item){
+    console.log(Math.max(item.notaAvaliacao))
+  })
+  
+}
+obterNota(listaFilmes)
+
+let idade = [18, 42, 12, 15, 20];
+
+console.log(Math.max(...idade))
+
+
+
+/*const obterFilmes = (listaFilmes) => {
     listaFilmes.filter(function(item){
         let maiorNumero = 0
         for (let i = 0; i < item.notaAvaliacao.lenght; i++) {
@@ -35,7 +50,7 @@ const obterFilmes = (listaFilmes) => {
   
   console.log(obterFilmes(listaFilmes))
   
-  /*let listaExemplo2 = [1, 5, 2]
+let listaExemplo2 = [1, 5, 2]
   let novaLista2 = listaExemplo.filter(function(item) {
       const ehImpar = item % 2 == 1
       return ehImpar
