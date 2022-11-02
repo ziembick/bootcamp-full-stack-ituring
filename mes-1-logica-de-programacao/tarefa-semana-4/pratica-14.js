48a05592f36ce14c11287fe41f746ece19b81b95
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-02 19:16:21 -0300

    lista pratica 14

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-14.js
@@ -0,0 +1,26 @@
+let l = {
+    restaurante: "Turing Gourmet",
+    tipo: "Comida britânica",
+    avaliacoes: [
+      {
+        cliente: "Thais S.",
+        nota: 9.8
+      },
+      {
+        cliente: "Thales Gonçalves",
+        nota: 8.9
+      },
+      {
+        cliente: "José Lopes",
+        nota: 9.9
+      },
+      {
+        cliente: "Cristina Souza",
+        nota: 9.3
+      },
+      {
+        cliente: "Leo Garcia",
+        nota: 8.5
+      }
+    ]
+  }
