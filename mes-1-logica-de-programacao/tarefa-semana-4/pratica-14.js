21b918b53ec2875ab9086ed4cf80fcbeb6d39d18
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-03 14:00:33 -0300

    arrumando pratica 14

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-14.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-14.js
@@ -1,54 +1,43 @@
-let lista = {
-    restaurante: "Turing Gourmet",
-    tipo: "Comida britânica",
-    avaliacoes: [
-      {
-        cliente: "Thais S.",
-        nota: 9.8
-      },
-      {
-        cliente: "Thales Gonçalves",
-        nota: 8.9
-      },
-      {
-        cliente: "José Lopes",
-        nota: 9.9
-      },
-      {
-        cliente: "Cristina Souza",
-        nota: 9.3
-      },
-      {
-        cliente: "Leo Garcia",
-        nota: 8.5
-      }
-    ]
-  }
+let estabelecimento = {
+  restaurante: "Turing Gourmet",
+  tipo: "Comida britânica",
+  avaliacoes: [
+    {
+      cliente: "Thais S.",
+      nota: 9.8
+    },
+    {
+      cliente: "Thales Gonçalves",
+      nota: 8.9
+    },
+    {
+      cliente: "José Lopes",
+      nota: 9.9
+    },
+    {
+      cliente: "Cristina Souza",
+      nota: 9.3
+    },
+    {
+      cliente: "Leo Garcia",
+      nota: 8.5
+    }
+  ]
+}
 
-//////////////////////////////////////////////////////
-/* const notasAlunos = [
-    
-    { cliente: "Thais S", nota: 9.8 },
-    { cliente: "Thales Gonçalves",  nota: 8.9 },
-    { cliente: "José Lopes", nota: 9.9 },
-    { cliente: "Cristina Souza",  nota: 9.3 },
-    { cliente:"Leo Garcia", nota: 8.5 } 
-   ]
-   
-   const acum = [];
-   
-   notasAlunos.forEach(aluno => {
-     const match = acum.find(media => media.cliente === aluno.cliente);
-     if (match) {
-       match.soma += aluno.nota
-       match.notas++;
-     } else {
-       aluno.soma = aluno.nota
-       aluno.notas = 1;
-       acum.push(aluno)
-     }
-   });
-   
-   const medias = [].map.call(acum, i => ({cliente: i.cliente, media: i.soma / i.notas}))
-   
-   console.log(medias)*/
+let mediaNota = function (estabelecimento)  {
+  let soma = 0;
+  let i = 0;
+estabelecimento.avaliacoes.forEach(function(item) {
+      soma += item.nota;
+      i++;
+    })
+
+  media = soma / i;
+  console.log(`A média de avaliações do restaurante Turing Gourmet foi de ${media.toFixed(2)}`);
+
+ }
+
+
+
+mediaNota(estabelecimento);
