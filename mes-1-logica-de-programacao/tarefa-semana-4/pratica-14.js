c0500588b144058fcc3f0768552573ae9bba078f
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-02 21:09:37 -0300

    iniciando pratica 14

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-14.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-14.js
@@ -1,4 +1,4 @@
-let l = {
+let lista = {
     restaurante: "Turing Gourmet",
     tipo: "Comida britânica",
     avaliacoes: [
@@ -24,3 +24,39 @@ let l = {
       }
     ]
   }
+
+
+
+const obterMedia = (media) => {
+    media.forEach(function(item){
+        
+    })
+}
+
+//////////////////////////////////////////////////////
+ const notasAlunos = [
+    
+    { cliente: "Thais S", nota: 9.8 },
+    { cliente: "Thales Gonçalves",  nota: 8.9 },
+    { cliente: "José Lopes", nota: 9.9 },
+    { cliente: "Cristina Souza",  nota: 9.3 },
+    { cliente:"Leo Garcia", nota: 8.5 } 
+   ]
+   
+   const acum = [];
+   
+   notasAlunos.forEach(aluno => {
+     const match = acum.find(media => media.cliente === aluno.cliente);
+     if (match) {
+       match.soma += aluno.nota
+       match.notas++;
+     } else {
+       aluno.soma = aluno.nota
+       aluno.notas = 1;
+       acum.push(aluno)
+     }
+   });
+   
+   const medias = [].map.call(acum, i => ({cliente: i.cliente, media: i.soma / i.notas}))
+   
+   console.log(medias)
