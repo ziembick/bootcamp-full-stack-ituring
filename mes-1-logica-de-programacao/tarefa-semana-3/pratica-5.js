6bd4d7f056ddd702156419c71b6989d85911c348
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-05 16:26:31 -0300

    pratica 15 console log refeito

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
@@ -18,21 +18,8 @@ function exibirSeriados (lista, termoDeBusca)  {
 
     const lista = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"];
    
-    const termoDeBusca = "Dexter";
-    //const termoDeBusca = "Friends"
 
-    console.log(exibirSeriados(lista, termoDeBusca));
+    console.log(exibirSeriados(lista, "Dexter"));
+    console.log(exibirSeriados(lista, "Friends"));
     
 ////////////////////////////////
-/*/* SOLUÇÃO 1 */
-let procuraItemEmListaS1 = (lista, valor) => {
-    for (var i = 0; i < lista.length; i++)
-        if (lista[i] === valor)
-            return i
-    return undefined
-}
-
-/* TESTANDO AS SOLUÇÕES */
-const listaSeriados = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
-console.log(procuraItemEmListaS1(listaSeriados, "Game of Thrones"))
-console.log(procuraItemEmListaS1(listaSeriados, "Friends"))
