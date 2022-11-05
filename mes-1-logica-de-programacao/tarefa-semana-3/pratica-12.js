762912edd862d8fb305f0d91d72e8420205ba62d
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-05 16:56:31 -0300

    pratica 12 solucionada

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-12.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-12.js
@@ -2,8 +2,16 @@ function geraGruposDe2(lista) {
     const qtdGrupos = Math.floor(lista.length/2);
     let grupos = [];
 
-    for (let i=0; i<qtdGrupos; i++) {
-        grupos.push(lista.slice(i*2, i*2+2))
+    for (let i=0; i <= qtdGrupos; i++) {
+        const indiceInicio = i*2;
+        const indiceFinal = i*2+2
+        grupos.push(lista.slice(indiceInicio, indiceFinal))
+    }
+
+    if (lista.length % 2 != 0) {
+        const indiceUltimoGrupo = grupos.length-1
+        const indiceUltimaPessoa = lista.length-1
+        grupos[indiceUltimoGrupo].push(lista[indiceUltimaPessoa]);
     }
 
     return grupos
@@ -12,4 +20,4 @@ function geraGruposDe2(lista) {
 
 let l = ["Anderson Luz", "Fabio Vechia", "Claudia Lopes", "Rafael Alves", "Maria Mansano", "Jairo Ferreira", "Vitor Ribeiro"]
 
-geraGruposDe2(l)
+console.table(geraGruposDe2(l))
