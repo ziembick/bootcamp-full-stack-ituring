3e0d3c20f9a5395b2b722a026bc86aaa3fbd0077
Paulo Ziembick <pauloziembick@gmail.com>
2022-11-05 16:25:02 -0300

    pratica 10 resulucionada

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-10.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-10.js
@@ -1,3 +1,4 @@
+///// minha solução
 let votosCopa = (lista) => {
     let brasil = 0
     let argentina = 0
@@ -43,3 +44,45 @@ let preferidoGanharCopa = (brasil, argentina, espanha, portugal) => {
 
 
 votosCopa([1, 1, 1, 4, 4, 2, 3, 2])
+///////////////////////////////////////////////////////
+
+
+/* SOLUÇÃO */
+let verificaPreferidoDaCopa = (votosBrasil, votosArgentina, votosEspanha, votosPortugal) => {
+    if (votosBrasil >= votosArgentina && votosBrasil >= votosEspanha && votosBrasil >= votosPortugal)
+        return "Brasil"
+    else if (votosArgentina >= votosEspanha && votosArgentina >= votosPortugal)
+        return "Argentina"
+    else if (votosEspanha >= votosPortugal)
+        return "Espanha"
+    else
+        return "Portugal"
+}
+
+let exibePreferidoDaCopa = listaVotos => {
+    let votosBrasil = 0
+    let votosArgentina = 0
+    let votosEspanha = 0
+    let votosPortugal = 0
+
+    for (voto of listaVotos) {
+        votosBrasil += voto === 1 ? 1 : 0
+        votosArgentina += voto === 2 ? 1 : 0
+        votosEspanha += voto === 3 ? 1 : 0
+        votosPortugal += voto === 4 ? 1 : 0
+    }
+    
+    var preferido = verificaPreferidoDaCopa(votosBrasil, votosArgentina, votosEspanha, votosPortugal)
+
+    console.log(`Brasil: ${votosBrasil} voto(s)`)
+    console.log(`Argentina: ${votosArgentina} voto(s)`)
+    console.log(`Espanha: ${votosEspanha} voto(s)`)
+    console.log(`Portugal: ${votosPortugal} voto(s)\n`)
+
+    console.log("Resultado:")
+    console.log(`${preferido} é o preferido para ganhar a Copa do Mundo!`)
+}
+
+/* TESTANDO A SOLUÇÃO */
+let listaVotos = [1, 1, 1, 4, 4, 2, 3, 2]
+exibePreferidoDaCopa(listaVotos)
